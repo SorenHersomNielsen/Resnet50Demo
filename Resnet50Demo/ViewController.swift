81c5c3cc0183266b855e7f001302bfd48569859b
SorenHersomNielsen <hersom@ellekrattet.dk>
2021-12-01 14:40:04 +0100

    Initial Commit

---
+++ Resnet50Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Resnet50Demo
+//
+//  Created by Søren Nielsen on 01/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
